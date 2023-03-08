d433bf4f5e944d3e8d1ea510471df47c5bfe40b8
Yonas Tamirat <120226721+Yonassiyum@users.noreply.github.com>
2023-03-08 13:33:05 -0500

    initial commit

--- src/Component/axios.js
+++ src/Component/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://us-central1-fir-2ad56.cloudfunctions.net/api",
-});//http://127.0.0.1:5001/fir-2ad56/us-central1/api
+	baseURL: "",
+});
 
 export default instance;
-
-
-
